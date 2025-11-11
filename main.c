e6b2b4e68dda787d55972c2191bb550faab732ec
ihebdalii <houbauniversity@gmail.com>
2025-11-11 15:26:21 +0100

    Menus , Ajout et affichage des livres

--- main.c
+++ main.c
@@ -1,14 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int T_NLivre[30];
-char T_Titre[30][30];
-int T_Nbr_exp[30];
+int T_NLivre[100];
+char T_Titre[100][50];
+int T_Nbr_exp[100];
+int N = 5;
 
 void afficherMenuPrincipal();
 void gestBiblio();
 void gestEmprunts();
 void stats();
+void afficherLivres(int T_NLivre[], char T_Titre[][50], int T_Nbr_exp[], int *N);
+void ajouterLivre(int T_NLivre[], char T_Titre[][50], int T_Nbr_exp[], int *N);
+
+// Les Livres initiaux
+
+void initialiserLivres(int T_NLivre[], char T_Titre[][50], int T_Nbr_exp[], int *N)
+{
+
+    *N = 5;
+    T_NLivre[0] = 101;
+    strcpy(T_Titre[0], "Le Petit Prince");
+    T_Nbr_exp[0] = 3;
+
+    T_NLivre[1] = 102;
+    strcpy(T_Titre[1], "Les Miserables");
+    T_Nbr_exp[1] = 5;
+
+    T_NLivre[2] = 103;
+    strcpy(T_Titre[2], "Atomic Habits");
+    T_Nbr_exp[2] = 3;
+
+    T_NLivre[3] = 104;
+    strcpy(T_Titre[3], "The Psychology of Money");
+    T_Nbr_exp[3] = 7;
+
+    T_NLivre[4] = 105;
+    strcpy(T_Titre[4], "The 48 Laws Of Power");
+    T_Nbr_exp[4] = 1;
+}
 
 void afficherMenuPrincipal()
 {
@@ -37,10 +68,47 @@ void afficherMenuPrincipal()
         case 3:
             stats();
             break;
-        default:
+        case 0:
+            printf("Merci d'avoir utilisé le système !...");
             break;
+        default:
+            printf("Choix invalide !\n");
         }
     } while (choix != 0);
+}
+
+void ajouterLivre(int T_NLivre[], char T_Titre[][50], int T_Nbr_exp[], int *N)
+{
+    int exp;
+    char c;
+    do
+    {
+        T_NLivre[*N] = 100 + (*N + 1);
+        getchar();
+
+        printf("Donnez le titre du livre : ");
+        fgets(T_Titre[*N], 50, stdin);
+        T_Titre[*N][strcspn(T_Titre[*N], "\n")] = '\0';
+
+        printf("Entrez le nombre d'exemplaires : ");
+        scanf("%d", &T_Nbr_exp[*N]);
+
+        (*N)++;
+
+        printf("Voulez-vous ajouter un autre livre ? (O/N) : ");
+        scanf(" %c", &c);
+    } while (c == 'O' || c == 'o');
+}
+void afficherLivres(int T_NLivre[], char T_Titre[][50], int T_Nbr_exp[], int *N)
+{
+    printf("╔════════════════════════════════════════════════════════╗\n");
+    printf("║                   LES LIVRES DISPONIBLES               ║\n");
+    printf("╠════════════════════════════════════════════════════════╣\n");
+    for (int i = 0; i < *N; i++)
+    {
+        printf("║            %d . %s : %d                                \n", T_NLivre[i], T_Titre[i], T_Nbr_exp[i]);
+    }
+    printf("╚════════════════════════════════════════════════════════╝\n");
 }
 
 void gestBiblio()
@@ -62,6 +130,18 @@ void gestBiblio()
         printf("╚════════════════════════════════════════════════════════╝\n");
         printf("|| == > ");
         scanf("%d", &choix);
+        switch (choix)
+        {
+        case 1:
+            afficherLivres(T_NLivre, T_Titre, T_Nbr_exp, &N);
+            break;
+        case 2:
+            ajouterLivre(T_NLivre, T_Titre, T_Nbr_exp, &N);
+            break;
+
+        default:
+            break;
+        }
     } while (choix != 0);
 }
 
@@ -111,6 +191,7 @@ void stats()
 
 int main()
 {
+    initialiserLivres(T_NLivre, T_Titre, T_Nbr_exp, &N);
     afficherMenuPrincipal();
     return 0;
 }
